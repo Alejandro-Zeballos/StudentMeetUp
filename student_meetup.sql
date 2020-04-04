5ca2a7c506d81a313639568927a7d7c9a368970c
DeCamposFiorelo <50555614+DeCamposFiorelo@users.noreply.github.com>
2020-04-04 22:02:18 +0100

    Add files via upload

--- student_meetup.sql
+++ student_meetup.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2020 at 07:18 PM
+-- Generation Time: Apr 04, 2020 at 10:59 PM
 -- Server version: 10.1.35-MariaDB
 -- PHP Version: 7.2.9
 
@@ -47,6 +47,7 @@ CREATE TABLE `session` (
   `id_session` int(11) NOT NULL,
   `course` varchar(20) NOT NULL,
   `session_title` varchar(220) NOT NULL,
+  `session_id_admin` int(11) NOT NULL,
   `session_admin` varchar(220) NOT NULL,
   `session_date` date NOT NULL,
   `session_time` time NOT NULL,
@@ -59,11 +60,10 @@ CREATE TABLE `session` (
 -- Dumping data for table `session`
 --
 
-INSERT INTO `session` (`id_session`, `course`, `session_title`, `session_admin`, `session_date`, `session_time`, `session_location`, `session_tags`, `session_descr`) VALUES
-(33, '', 'PHP', 'Sue', '2020-03-31', '21:10:00', 'Library', 'PHP', 'Hi guys, I\'ll be in the library today studying PHP. If anyone would like to join me (:'),
-(34, '', 'JAVA', '', '2020-03-31', '21:15:00', 'Library', '62S6', 'Hi guys, I\'ll be in the library today studying JAVA. If anyone would like to join me (:'),
-(35, '', 'NETWORK', '', '2020-03-31', '20:00:00', 'Library', '2s2s', 'Hi guys, I\'ll be in the library today studying Network. If anyone would like to join me (:'),
-(41, '', 'JAVASCRIPT', '', '2020-04-02', '16:00:00', 'Library', 'javascript,web', 'Hi guys, I\'ll be in the library today studying JavaScript. If anyone would like to join me (:');
+INSERT INTO `session` (`id_session`, `course`, `session_title`, `session_id_admin`, `session_admin`, `session_date`, `session_time`, `session_location`, `session_tags`, `session_descr`) VALUES
+(63, 'IT', 'JAVASCRIPT', 7, 'Suelen', '2020-04-04', '21:22:00', 'ss', 'ss', 'ss'),
+(64, 'Business', 'java', 7, 'Sue', '2020-04-04', '21:30:00', 'ss', 'ss', 'ss'),
+(65, 'Business', 'JAVASCRIPT', 19, 'admin', '2020-04-04', '21:45:00', 'aa', 'aa', 'aa');
 
 -- --------------------------------------------------------
 
@@ -74,7 +74,7 @@ INSERT INTO `session` (`id_session`, `course`, `session_title`, `session_admin`,
 CREATE TABLE `student` (
   `id_student` int(11) NOT NULL,
   `student_firstName` varchar(220) NOT NULL,
-  `student_lastName` varchar(220) NOT NULL,
+  `student_nickname` varchar(220) NOT NULL,
   `student_email` varchar(220) NOT NULL,
   `student_password` varchar(220) NOT NULL,
   `student_course` varchar(30) NOT NULL,
@@ -86,9 +86,10 @@ CREATE TABLE `student` (
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`id_student`, `student_firstName`, `student_lastName`, `student_email`, `student_password`, `student_course`, `student_description`, `student_foto`) VALUES
-(7, 'Suelen', '', 'suelenfiorelo@hotmail.com', '123', 'IT', 's', ''),
-(16, 'Teste', 'Teste', 'teste@hotmail.com', '$2y$12$6reFRasEqxKZOnPlvEWK0.3NiAgsziyEIRT1ge8180RWGBoalVpyO', 'IT', 'teste', '');
+INSERT INTO `student` (`id_student`, `student_firstName`, `student_nickname`, `student_email`, `student_password`, `student_course`, `student_description`, `student_foto`) VALUES
+(7, 'Suelen', 'Sue', 'suelenfiorelo@hotmail.com', '123', 'IT', 's', ''),
+(16, 'Teste', 'Teste', 'teste@hotmail.com', '$2y$12$6reFRasEqxKZOnPlvEWK0.3NiAgsziyEIRT1ge8180RWGBoalVpyO', 'IT', 'teste', ''),
+(24, 'Suelen', 'admin', 'admin@hotmail.com', '123', 'IT', '123', '');
 
 --
 -- Indexes for dumped tables
@@ -127,13 +128,13 @@ ALTER TABLE `report`
 -- AUTO_INCREMENT for table `session`
 --
 ALTER TABLE `session`
-  MODIFY `id_session` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id_session` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `id_student` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_student` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
